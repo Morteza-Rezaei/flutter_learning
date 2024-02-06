96d5a1b4a1598a224e828ed238b8579c7545ff84
Morteza <moradimurtaza84@gmail.com>
2024-02-06 16:27:21 +0300

    S9:Register section

--- ulearning_app/lib/main.dart
+++ ulearning_app/lib/main.dart
@@ -2,13 +2,10 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:ulearning_app/app_blocs.dart';
-import 'package:ulearning_app/app_events.dart';
-import 'package:ulearning_app/app_states.dart';
+import 'package:ulearning_app/common/values/colors.dart';
 import 'package:ulearning_app/pages/bloc_providers.dart';
-import 'package:ulearning_app/pages/sign_in/bloc/sign_in_blocs.dart';
+import 'package:ulearning_app/pages/register/register.dart';
 import 'package:ulearning_app/pages/sign_in/sign_in.dart';
-import 'package:ulearning_app/pages/welcome/bloc/welcome_blocs.dart';
 import 'package:ulearning_app/pages/welcome/welcome.dart';
 
 void main() async {
@@ -29,6 +26,9 @@ class MyApp extends StatelessWidget {
           theme: ThemeData(
             useMaterial3: true,
             appBarTheme: const AppBarTheme(
+              iconTheme: IconThemeData(
+                color: AppColors.primaryText,
+              ),
               elevation: 0,
               backgroundColor: Colors.white,
             ),
@@ -36,65 +36,10 @@ class MyApp extends StatelessWidget {
           debugShowCheckedModeBanner: false,
           home: const Welcome(),
           routes: {
-            'myHomePage': (context) => const MyHomePage(),
             'signIn': (context) => const SignIn(),
+            'register': (context) => const Register(),
           },
         ),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Fluttter demo home page'),
-      ),
-      body: Center(
-        child: BlocBuilder<AppBlocs, AppStates>(
-          builder: (context, state) {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text(
-                  'Your counter is:',
-                ),
-                Text(
-                  '${BlocProvider.of<AppBlocs>(context).state.counter}',
-                  style: Theme.of(context).textTheme.headlineMedium,
-                ),
-              ],
-            );
-          },
-        ),
-      ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          FloatingActionButton(
-            heroTag: 'heroTag1',
-            onPressed: () =>
-                BlocProvider.of<AppBlocs>(context).add(Increment()),
-            tooltip: 'Increment',
-            child: const Icon(Icons.add),
-          ),
-          FloatingActionButton(
-            heroTag: 'heroTag2',
-            onPressed: () =>
-                BlocProvider.of<AppBlocs>(context).add(Decrement()),
-            tooltip: 'Decrement',
-            child: const Icon(Icons.remove),
-          ),
-        ],
       ),
     );
   }
--- ulearning_app/lib/pages/sign_in/sign_in.dart
+++ ulearning_app/lib/pages/sign_in/sign_in.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ulearning_app/pages/common_widgets.dart';
 import 'package:ulearning_app/pages/sign_in/bloc/sign_in_blocs.dart';
 import 'package:ulearning_app/pages/sign_in/bloc/sign_in_events.dart';
 import 'package:ulearning_app/pages/sign_in/bloc/sign_in_states.dart';
 import 'package:ulearning_app/pages/sign_in/sign_in_controller.dart';
-import 'package:ulearning_app/pages/sign_in/widgets/sign_in_widget.dart';
 
 class SignIn extends StatefulWidget {
   const SignIn({super.key});
@@ -23,7 +23,7 @@ class _SignInState extends State<SignIn> {
           color: Colors.white,
           child: Scaffold(
               backgroundColor: Colors.white,
-              appBar: buildAppBar(),
+              appBar: buildAppBar('Log In'),
               body: SingleChildScrollView(
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
@@ -62,10 +62,12 @@ class _SignInState extends State<SignIn> {
                       ),
                     ),
                     forgotPassword(),
-                    buildLogInAndRegButton('Log in', 'Login', () {
+                    buildLogInAndRegButton('Log in', 'login', () {
                       SignInController(context: context).handleSignIn("email");
                     }),
-                    buildLogInAndRegButton('Register', 'Register', () {}),
+                    buildLogInAndRegButton('Sign Up', 'register', () {
+                      Navigator.of(context).pushNamed('register');
+                    }),
                   ],
                 ),
               )),
